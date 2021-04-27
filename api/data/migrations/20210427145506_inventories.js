bb3cc5131cf55e2fae41e73e16d18dff7f448b2a
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-27 12:34:41 -0400

    Add a stock collumn to inventories table in db

--- api/data/migrations/20210427145506_inventories.js
+++ api/data/migrations/20210427145506_inventories.js
@@ -3,6 +3,7 @@ exports.up = function (knex) {
     table.increments().primary();
     table.integer("book_id").notNullable().references("books.id");
     table.integer("store_id").notNullable().references("stores.id");
+    table.integer("stock").notNullable().defaultTo(0);
 
     table.unique(["book_id", "store_id"]);
   });
