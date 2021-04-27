d904582a22b7654b2003584c7dbbe094e45a8b0f
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-26 23:24:18 -0400

    Setup connection to local DB

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,32 +1,37 @@
 // Update with your config settings.
 
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: './data/migrations',
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
-    seeds: { directory: './data/seeds' },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
   },
 
   testing: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DB_URL,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
-    seeds: { directory: './data/seeds' },
+    seeds: { directory: "./data/seeds" },
   },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DB_URL,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
-    seeds: { directory: './data/seeds' },
+    seeds: { directory: "./data/seeds" },
   },
 };
