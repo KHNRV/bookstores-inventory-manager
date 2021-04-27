66c411f2e9e89c273fe1a567c8c7b869173ed3ab
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-27 15:32:53 -0400

    Create authentification helper file for jwt

---
+++ api/helpers/authentication.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = {
+  generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET);
+  },
+
+  authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      console.log(err);
+
+      if (err) return res.sendStatus(403);
+      req.user = user;
+
+      next();
+    });
+  },
+};
