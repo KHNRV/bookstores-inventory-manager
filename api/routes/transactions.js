e1a0a69d3c990e089ea7dc854ac16e528cb48bb3
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 12:12:28 -0400

    Add get routes for transactions index

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -1,7 +1,10 @@
 const transactions = require("express").Router();
 
 module.exports = (db, { authenticateToken }) => {
-  transactions.get("/", authenticateToken, async (req, res, next) => {});
+  transactions.get("/", authenticateToken, async (req, res, next) => {
+    const result = await db.transactions.index();
+    res.send(result);
+  });
 
   transactions.post("/", authenticateToken, async (req, res, next) => {});
   transactions.get("/:id", authenticateToken, async (req, res, next) => {});
