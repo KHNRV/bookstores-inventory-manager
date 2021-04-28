18bad456aa735a2aea89ef91525925a36541f000
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 15:35:03 -0400

    Create route to get inventories

--- api/routes/inventories.js
+++ api/routes/inventories.js
@@ -1,10 +1,23 @@
 const inventories = require("express").Router();
 
 module.exports = (db, { authenticateToken }) => {
-  inventories.get("/", authenticateToken, async (req, res, next) => {});
+  inventories.get("/", authenticateToken, async (req, res, next) => {
+    const response = await db.inventories.index();
+    res.send(response.rows);
+  });
 
   inventories.post("/", authenticateToken, async (req, res, next) => {});
-  inventories.get("/:id", authenticateToken, async (req, res, next) => {});
+  inventories.get(
+    "/:isbn_13/:store_id",
+    authenticateToken,
+    async (req, res, next) => {
+      const { isbn_13, store_id } = req.params;
+
+      const response = await db.inventories.read(isbn_13, store_id);
+
+      res.send(response.rows);
+    }
+  );
   inventories.put("/:id", authenticateToken, async (req, res, next) => {});
   inventories.delete("/:id", authenticateToken, async (req, res, next) => {});
   return inventories;
