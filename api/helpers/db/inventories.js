4e5ad43660b63c1f4d27967623e8a24f09ae0352
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 15:34:10 -0400

    Add inventories readers

--- api/helpers/db/inventories.js
+++ api/helpers/db/inventories.js
@@ -1,10 +1,27 @@
 module.exports = (knex) => ({
   index() {
-    return knex.select().from("inventories");
+    return knex.raw(
+      ` SELECT books.isbn_13, stores.id AS store_id ,  sum(transactions.change) AS stock
+        FROM stores, books, transactions
+        WHERE transactions.store_id = stores.id
+        AND transactions.book_id = books.id
+        GROUP BY stores.id, books.isbn_13, books.title`
+    );
   },
 
   create() {},
-  read() {},
+  read(isbn_13, store_id) {
+    return knex.raw(
+      ` SELECT books.isbn_13, stores.id AS store_id ,  sum(transactions.change) AS stock
+        FROM stores, books, transactions
+        WHERE transactions.store_id = stores.id
+        AND transactions.book_id = books.id
+        AND books.isbn_13 = ?
+        AND stores.id = ?
+        GROUP BY stores.id, books.isbn_13, books.title`,
+      [isbn_13, store_id]
+    );
+  },
   update() {},
   destroy() {},
 });
