5693be158eaee64e9485f3a97077ab09748f2d80
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 15:35:59 -0400

    Prevent the stock to go below zero

--- api/routes/transactions.js
+++ api/routes/transactions.js
@@ -6,7 +6,28 @@ module.exports = (db, { authenticateToken }) => {
     res.send(result);
   });
 
-  transactions.post("/", authenticateToken, async (req, res, next) => {});
+  transactions.post("/", authenticateToken, async (req, res, next) => {
+    const newTransaction = req.body;
+    const { book_id, store_id } = newTransaction;
+    const isbn_13 = db.books.get.isbn_13.from.id(book_id);
+    try {
+      const projectedStock = await db.inventories.read(isbn_13, store_id);
+      projectedStock.rows[0].stock += newTransaction.change;
+
+      if (projectedStock.rows[0].stock >= 0) {
+        const response = await db.transactions.create(newTransaction);
+        res.send(response);
+      } else {
+        throw {
+          type: "ERROR",
+          message: "Stock too low for this operation to succeed",
+          projectedStock: projectedStock.rows[0].stock,
+        };
+      }
+    } catch (error) {
+      res.status(403).send(error);
+    }
+  });
   transactions.get("/:id", authenticateToken, async (req, res, next) => {});
   transactions.put("/:id", authenticateToken, async (req, res, next) => {});
   transactions.delete("/:id", authenticateToken, async (req, res, next) => {});
