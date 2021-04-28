6b78d42d64d06ea57dfebede8cdbc2e5c893ffb8
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 00:57:50 -0400

    Add get route for book

--- api/routes/books.js
+++ api/routes/books.js
@@ -7,7 +7,11 @@ module.exports = (db, { authenticateToken }) => {
   });
 
   books.post("/", authenticateToken, async (req, res, next) => {});
-  books.get("/:id", authenticateToken, async (req, res, next) => {});
+  books.get("/:isbn_13", authenticateToken, async (req, res, next) => {
+    const isbn_13 = req.params.isbn_13;
+    const result = await db.books.read().where.isbn_13(isbn_13);
+    res.send(result);
+  });
   books.put("/:id", authenticateToken, async (req, res, next) => {});
   books.delete("/:id", authenticateToken, async (req, res, next) => {});
 
