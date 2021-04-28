e65b1dbf620b2c7d24efaf76517ee29db96ccbe2
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 12:13:19 -0400

    Add test for POST books method

--- api/routes/__tests__/books.spec.js
+++ api/routes/__tests__/books.spec.js
@@ -8,60 +8,78 @@ const username = process.env.DB_USER;
 const token = generateAccessToken(username);
 
 beforeAll(async () => {
+  await db.migrate.rollback();
   await db.migrate.latest();
+  await db.seed.run();
 });
 
-it("GET    >> /api/books", async (done) => {
-  // Sends GET Request to /test endpoint
-  const expectedResults = [
-    {
+describe("Books", () => {
+  it("GET    >> /api/books", async (done) => {
+    // Sends GET Request to /test endpoint
+    const expectedResults = [
+      {
+        author_id: 1,
+        id: 1,
+        isbn_13: "9780446675536",
+        title: "Gone with the Wind",
+      },
+      { author_id: 2, id: 2, isbn_13: "9781551111803", title: "Jane Eyre" },
+      {
+        author_id: 3,
+        id: 3,
+        isbn_13: "9780553213102",
+        title: "Pride and Prejudice",
+      },
+      { author_id: 3, id: 4, isbn_13: "9780141439587", title: "Emma" },
+    ];
+
+    const res = await request
+      .get("/api/books")
+      .set("Authorization", `${username} ${token}`);
+
+    expect(res.body).toEqual(expectedResults);
+    done();
+  });
+
+  it("POST   >> /api/books", async (done) => {
+    const newBook = {
+      title: "The Professor",
+      isbn_13: "9781582870953",
+      author_id: 2,
+    };
+
+    const res = await request
+      .post(`/api/books/`)
+      .send(newBook)
+      .set("Authorization", `${username} ${token}`)
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res.body).toEqual([{ id: 5 }]);
+    done();
+  });
+
+  it("GET    >> /api/books/:isbn_13", async (done) => {
+    const expectedResults = {
       author_id: 1,
       id: 1,
       isbn_13: "9780446675536",
       title: "Gone with the Wind",
-    },
-    { author_id: 2, id: 2, isbn_13: "9781551111803", title: "Jane Eyre" },
-    {
-      author_id: 3,
-      id: 3,
-      isbn_13: "9780553213102",
-      title: "Pride and Prejudice",
-    },
-    { author_id: 3, id: 4, isbn_13: "9780141439587", title: "Emma" },
-  ];
+    };
 
-  const res = await request
-    .get("/api/books")
-    .set("Authorization", `${username} ${token}`);
-  // ...
-  expect(res.body).toEqual(expectedResults);
-  done();
-});
+    const res = await request
+      .get(`/api/books/${expectedResults.isbn_13}`)
+      .set("Authorization", `${username} ${token}`);
+    // ...
+    expect(res.body).toEqual(expectedResults);
+    done();
+  });
 
-xit("POST   >> /api/books", async (done) => {
-  done();
-});
+  xit("PUT    >> /api/books/:id", async (done) => {
+    done();
+  });
 
-it("GET    >> /api/books/:isbn_13", async (done) => {
-  const expectedResults = {
-    author_id: 1,
-    id: 1,
-    isbn_13: "9780446675536",
-    title: "Gone with the Wind",
-  };
-
-  const res = await request
-    .get(`/api/books/${expectedResults.isbn_13}`)
-    .set("Authorization", `${username} ${token}`);
-  // ...
-  expect(res.body).toEqual(expectedResults);
-  done();
-});
-
-xit("PUT    >> /api/books/:id", async (done) => {
-  done();
-});
-
-xit("DELETE >> /api/books/:id", async (done) => {
-  done();
+  xit("DELETE >> /api/books/:id", async (done) => {
+    done();
+  });
 });
