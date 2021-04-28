7f7801a7ee2ee18cad6e236d682aa0fbd1c77770
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-28 12:12:10 -0400

    Add GET and POST routes for books

--- api/routes/books.js
+++ api/routes/books.js
@@ -2,15 +2,23 @@ const books = require("express").Router();
 
 module.exports = (db, { authenticateToken }) => {
   books.get("/", authenticateToken, async (req, res, next) => {
-    const result = await db.books.index();
-    res.send(result);
+    const response = await db.books.index();
+    res.send(response);
   });
 
-  books.post("/", authenticateToken, async (req, res, next) => {});
+  books.post("/", authenticateToken, async (req, res, next) => {
+    const newBook = req.body;
+    try {
+      const response = await db.books.create(newBook);
+      res.send(response);
+    } catch (error) {
+      res.status(403).send(error);
+    }
+  });
   books.get("/:isbn_13", authenticateToken, async (req, res, next) => {
     const isbn_13 = req.params.isbn_13;
-    const result = await db.books.read().where.isbn_13(isbn_13);
-    res.send(result);
+    const response = await db.books.read().where.isbn_13(isbn_13);
+    res.send(response);
   });
   books.put("/:id", authenticateToken, async (req, res, next) => {});
   books.delete("/:id", authenticateToken, async (req, res, next) => {});
